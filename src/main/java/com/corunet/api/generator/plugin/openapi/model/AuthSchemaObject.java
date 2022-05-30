26f0e270ce4e150c7f15f7fc00c794b50464ce2f
fcampos <fcampos@corunet.com>
2022-05-30 12:07:51 +0200

    Add license header to all classes

--- src/main/java/com/corunet/api/generator/plugin/openapi/model/AuthSchemaObject.java
+++ src/main/java/com/corunet/api/generator/plugin/openapi/model/AuthSchemaObject.java
@@ -1,3 +1,9 @@
+/*
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  * License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.corunet.api.generator.plugin.openapi.model;
 
 import lombok.Builder;
@@ -16,7 +22,6 @@ public class AuthSchemaObject {
   private String apiKeyPlace;
 
   private String apiKeyParam;
-
 
 
 }
