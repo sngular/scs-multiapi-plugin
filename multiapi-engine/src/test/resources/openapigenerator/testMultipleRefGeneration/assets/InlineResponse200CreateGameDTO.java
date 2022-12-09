26f46722817021ba76f2a7828a103b1d23189ed2
Adrian Garcia Maciñeiras <Adrian.garciam@udc.es>
2022-12-09 13:20:19 +0100

    Fix Template Issue

--- multiapi-engine/src/test/resources/openapigenerator/testMultipleRefGeneration/assets/InlineResponse200CreateGameDTO.java
+++ multiapi-engine/src/test/resources/openapigenerator/testMultipleRefGeneration/assets/InlineResponse200CreateGameDTO.java
@@ -11,16 +11,20 @@ public class InlineResponse200CreateGameDTO {
   private String description;
   @JsonProperty(value ="code")
   private Integer code;
+  @JsonProperty(value ="message")
+  private MessageDTO message;
 
-  private InlineResponse200CreateGameDTO(String description, Integer code) {
+  private InlineResponse200CreateGameDTO(String description, Integer code, MessageDTO message) {
     this.description = description;
     this.code = code;
+    this.message = message;
 
   }
 
   private InlineResponse200CreateGameDTO(InlineResponse200CreateGameDTOBuilder builder) {
     this.description = builder.description;
     this.code = builder.code;
+    this.message = builder.message;
 
   }
 
@@ -28,6 +32,7 @@ public class InlineResponse200CreateGameDTO {
 
     private String description;
     private Integer code;
+    private MessageDTO message;
 
     public InlineResponse200CreateGameDTO.InlineResponse200CreateGameDTOBuilder description(String description) {
       this.description = description;
@@ -36,6 +41,10 @@ public class InlineResponse200CreateGameDTO {
 
     public InlineResponse200CreateGameDTO.InlineResponse200CreateGameDTOBuilder code(Integer code) {
       this.code = code;
+      return this;
+    }
+    public InlineResponse200CreateGameDTO.InlineResponse200CreateGameDTOBuilder message(MessageDTO message) {
+      this.message = message;
       return this;
     }
 
@@ -69,6 +78,18 @@ public class InlineResponse200CreateGameDTO {
     this.code = code;
   }
 
+  /**
+  * Get message
+  * @return message
+  */
+  @Schema(name = "message", required = false)
+  public MessageDTO getMessage() {
+    return message;
+  }
+  public void setMessage(MessageDTO message) {
+    this.message = message;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) {
@@ -78,12 +99,12 @@ public class InlineResponse200CreateGameDTO {
       return false;
     }
     InlineResponse200CreateGameDTO inlineResponse200CreateGameDTO = (InlineResponse200CreateGameDTO) o;
-    return Objects.equals(this.description, inlineResponse200CreateGameDTO.description) && Objects.equals(this.code, inlineResponse200CreateGameDTO.code);
+    return Objects.equals(this.description, inlineResponse200CreateGameDTO.description) && Objects.equals(this.code, inlineResponse200CreateGameDTO.code) && Objects.equals(this.message, inlineResponse200CreateGameDTO.message);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(description, code);
+    return Objects.hash(description, code, message);
   }
 
   @Override
@@ -92,6 +113,7 @@ public class InlineResponse200CreateGameDTO {
     sb.append("class InlineResponse200CreateGameDTO {\n");
     sb.append(" description: ").append(toIndentedString(description)).append("\n");
     sb.append(" code: ").append(toIndentedString(code)).append("\n");
+    sb.append(" message: ").append(toIndentedString(message)).append("\n");
     sb.append("}");
     return sb.toString();
   }
