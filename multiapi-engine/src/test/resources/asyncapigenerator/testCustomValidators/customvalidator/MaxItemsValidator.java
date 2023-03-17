b8c34a39b976b49295c7cc15e832a754db248afb
davidgayoso <david.gayoso@sngular.com>
2023-03-17 13:01:51 +0100

    #178 fixed error with MinItems and MaxItems validators

--- multiapi-engine/src/test/resources/asyncapigenerator/testCustomValidators/customvalidator/MaxItemsValidator.java
+++ multiapi-engine/src/test/resources/asyncapigenerator/testCustomValidators/customvalidator/MaxItemsValidator.java
@@ -1,20 +1,26 @@
 package com.sngular.scsplugin.customvalidator.model.event.customvalidator;
 
+import java.util.List;
+import java.util.Objects;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class MaxItemsValidator implements ConstraintValidator<MaxItems, Array> {
+public class MaxItemsValidator implements ConstraintValidator<MaxItems, List<?>> {
 
   private int maximum;
 
   @Override
   public void initialize(MaxItems constraintAnnotation) {
     ConstraintValidator.super.initialize(constraintAnnotation);
-    this.maximum = constraintAnnotation.maximum()
+    this.maximum = constraintAnnotation.maximum();
   }
 
   @Override
-  public boolean isValid(Array value, ConstraintValidatorContext context) {
+  public boolean isValid(List<?> value, ConstraintValidatorContext context) {
+    if (Objects.isNull(value))
+      return true;
+
     if (value.size() > this.maximum) {
       return false;
     } else {
