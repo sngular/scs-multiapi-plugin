8c2da3049c063007122d4e18d05ef85f8d37594e
Suso <126153639+5uso-sng@users.noreply.github.com>
2023-05-12 13:49:35 +0200

    #238 AsyncAPI generate inlined message schemas (#240)
    * #238 AsyncAPI test no schemas
    
    * #238 Update test
    
    * #238 Ensure message inlined schemas are generated
    
    * Update version
    
    * Shorten channel schema collection
    
    * Remove redundant logic

---
+++ multiapi-engine/src/test/resources/asyncapigenerator/testNoSchemas/assets/Thing.java
@@ -0,0 +1,97 @@
+package com.sngular.scsplugin.noschemas.model.schemas;
+
+import java.util.Objects;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@JsonDeserialize(builder = Thing.ThingBuilder.class)
+public class Thing {
+
+  @JsonProperty(value ="id")
+  private Integer id;
+
+  private Thing(Integer id) {
+    this.id = id;
+
+  }
+
+  private Thing(ThingBuilder builder) {
+    this.id = builder.id;
+
+  }
+
+  public static Thing.ThingBuilder builder() {
+    return new Thing.ThingBuilder();
+  }
+
+  @JsonPOJOBuilder(buildMethodName = "build", withPrefix = "")
+  public static class ThingBuilder {
+
+    private Integer id;
+
+    public Thing.ThingBuilder id(Integer id) {
+      this.id = id;
+      return this;
+    }
+
+    public Thing build() {
+      Thing thing = new Thing(this);
+      return thing;
+    }
+  }
+
+  /**
+  * Get id
+  * @return id
+  */
+  @Schema(name = "id", required = false)
+  public Integer getId() {
+    return id;
+  }
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Thing thing = (Thing) o;
+    return Objects.equals(this.id, thing.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Thing {\n");
+    sb.append(" id: ").append(toIndentedString(id)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+  * Convert the given object to string with each line indented by 4 spaces
+  * (except the first line).
+  */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n ");
+  }
+
+
+
+}
